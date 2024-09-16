16904b0a8b7a16611cde0a9d0b32b9ec1ef454eb
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-17 00:11:48 +0500

    added API NewsAPI

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 # create a function with a variable weather, where we will store the weather
 def index():
    weather = None
+   news = None
 # we form conditions for testing the method.
 # We haven't created the form yet, but we'll only need to take the city from it.
    if request.method == 'POST':
@@ -17,12 +18,21 @@ def index():
        city = request.form['city']
        # we register a variable where the result will be saved
        # weather function with the indication of the city, which we take from the form
+
        weather = get_weather(city)
        if weather:
            print("weather: ", weather)
        else:
            print("Failed to retrieve weather data")
-   return render_template("index.html", weather=weather)
+
+       news = get_news()
+       if news:
+           print("news: ", news)
+       else:
+           print("Failed to retrieve news data")
+
+
+   return render_template("index.html", weather=weather, news=news)
 
 # in the function we write the city that we will enter in the form
 def get_weather(city):
@@ -41,6 +51,19 @@ def get_weather(city):
    else:
        print("Error while receiving data:", response.status_code)
        return None
+
+def get_news():
+   api_key = "216a06544d934b8a9286726c3e87577f"
+   url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+   response = requests.get(url)
+   # return response.json().get('articles', [])
+   if response.status_code == 200:
+       # we write the format of the result return
+       return response.json().get('articles', [])
+   else:
+       print("Error while receiving data:", response.status_code)
+       return None
+
 
 if __name__ == '__main__':
    app.run(debug=True)
